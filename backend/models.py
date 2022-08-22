31aeabebbe6af74659760f9440493bfb5369bb1d
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-22 14:59:00 +0100

    feat: update database and user credentials

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,10 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_name = "trivia_db"
+database_path = "postgresql://{}:{}@{}/{}".format(
+    "postgres", "password", "localhost:5432", database_name
+)
 
 db = SQLAlchemy()
 
