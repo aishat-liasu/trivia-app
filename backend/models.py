226e10d7077e165b1d51d94be5c89544c241c537
aishat-liasu <aishatliasu17965@gmail.com>
2022-08-23 17:17:39 +0100

    refactor: remove additional spacing

--- backend/models.py
+++ backend/models.py
@@ -12,14 +12,14 @@ db = SQLAlchemy()
 
 '''
 setup_db(app)
-    binds a flask application and a SQLAlchemy service
+binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.app = app
+  db.init_app(app)
+  db.create_all()
 
 '''
 Question
